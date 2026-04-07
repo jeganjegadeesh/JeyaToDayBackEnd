1887a2d9a8aa898716a408d4979473f4d2e8a622
Jegadeesh <jegadeesh2242@gmail.com>
2026-04-07 12:50:55 +0530

    text succes commet

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,7 +37,7 @@ class AuthController extends Controller
     {
         $request->user()->currentAccessToken()->delete();
 
-        return response()->json(['message' => 'Logged out successfull.']);
+        return response()->json(['message' => 'Logged out success.']);
     }
 
     /**
