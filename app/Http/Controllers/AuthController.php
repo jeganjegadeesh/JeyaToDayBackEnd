30329ac5fa43a7e5b97cbf999fcd67e1cf26625f
Jegadeesh <jegadeesh2242@gmail.com>
2026-04-07 12:38:43 +0530

    text commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,7 +37,7 @@ class AuthController extends Controller
     {
         $request->user()->currentAccessToken()->delete();
 
-        return response()->json(['message' => 'Logged out successfully.']);
+        return response()->json(['message' => 'Logged out successfull.']);
     }
 
     /**
